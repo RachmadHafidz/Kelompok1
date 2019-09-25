9b4715716478c914864f92c004740e236365c93e
Hafil123 <55471270+Hafil123@users.noreply.github.com>
2019-09-25 10:31:18 +0700

    Create tipedata.php

---
+++ hafil/tipedata.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$tes = "Saya Pergi Kerja";
+echo "<h2>" .$tes. "</h2>"
+// variabel tes di atas merupakan tipe data string karena berisi text atau kalimat.
+?>
+</body>
+</html>
+
