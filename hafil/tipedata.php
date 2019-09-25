a97b310fe1e0d0d9fd7affee2bf82c6d64ba59a4
arinifir <55473280+arinifir@users.noreply.github.com>
2019-09-25 13:44:39 +0700

    Update tipedata.php

--- hafil/tipedata.php
+++ hafil/tipedata.php
@@ -6,25 +6,6 @@
 $tes = "Saya Pergi Kerja";
 echo "<h2>" .$tes. "</h2>"
 ?>
-<?php 
-$bilangan_pertama = 12;
-$bilangan_kedua = 78;
-$hasil = $bilangan_pertama * $bilangan_kedua
-echo "<h2>" .$hasil."</h2>"
-?>
-<?php 
-$angka = 12.177;
-echo "<h2>" .$angka. "</h2>"
-?>
-<?php 
-$x = false;
-$y = true;
-echo "<h2>" .$y. "</h2>"
-?>
-<?php 
-$anggota = array("Andi","Budi","Joni");
-echo "<h2>" .$anggota. "</h2>"
-?>
 
 </body>
 </html>
