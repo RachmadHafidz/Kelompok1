b52d5b0c2800ec050422089f2f5952d136eb2893
arinifir <55473280+arinifir@users.noreply.github.com>
2019-09-25 14:41:31 +0700

    Create home.php

---
+++ arin/home.php
@@ -0,0 +1,11 @@
+<?php
+   ob_start();
+    session_start();
+     ob_end_clean();
+    if(isset($_SESSION["email"])){
+    echo "<h2>Selamat Anda Berhasil Login</h2> <h3>Klik Logout Dibawah Ini";
+    echo "<a href='logout.php'><h4>Logout</h4></a>";
+    }else{
+        echo header("location:login.php");
+    }
+?> 
