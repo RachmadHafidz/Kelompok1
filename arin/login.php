9c71078ed73ac044ee98c87904a5d17ec469f7c6
arinifir <55473280+arinifir@users.noreply.github.com>
2019-09-25 14:41:17 +0700

    Create login.php

---
+++ arin/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Login Form</title>
+</head>
+<body>
+        
+    <?php 
+        if(isset($_GET["login_error"])){
+            echo "<h3 style='color:red';>Email atau password salah</h3>";
+        }
+    ?>
+      <h1>Login to Home</h1>
+      <form method="post" action="ceklogin.php">
+        <p><input type="text" name="email" value="" placeholder="Email"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+      </form>
+    </div>
+
+</body>
+</html> 
