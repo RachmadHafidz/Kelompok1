c66b776cd44c18bc559773ac62dac231a576fefe
rianjanuarian <55471319+rianjanuarian@users.noreply.github.com>
2019-10-09 09:12:43 +0700

    Update nyoba2.php

--- rian/selasa/nyoba2.php
+++ rian/selasa/nyoba2.php
@@ -7,62 +7,13 @@
 <title>S.I.D Sabrang</title>
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="stylenav.css">
 	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <style type="text/css">
-
-*{
- margin: 0;
- padding: 0;
- font-family: sans-serif;
-}
-
-#sidebar{
- position: fixed;
- width: 200px;
- height: 100%;
- background: #151719;
- left: -200px;
- transition: all 500ms linear;
-}
-
-#sidebar.active{
- left:0px;
-}
-
-#sidebar ul li{
- color: rgba(230,230,230,0.9);
- list-style: none;
- padding: 15px 10px;
- border-bottom: 1px solid rgba(100,100,100,0.3);
-}
-
-#sidebar .toggle-btn{
- position: absolute;
- left: 230px;
- top: 20px;
-}
-
-#sidebar .toggle-btn span{
- display: block;
- width: 30px;
- height: 5px;
- background: #151719;
- margin: 5px 0px;
-}
-
-
-
-</style>
-<script type="text/javascript">
-function toggleSidebar(){
- document.getElementById("sidebar").classList.toggle('active');
-}
-</script>
-
+ 
 
 </head>
 <body>
@@ -74,11 +25,21 @@ function toggleSidebar(){
 			</button>
       <nav class="navbar navbar-dark bg-dark justify-content-between"> <p><a><font class="navbar-brand" color="white">S.I.D</font></a></p>
         <p><font color="white">Jl. watu ulo no 1, Desa Sabrang, Kec. Ambulu, Kab. Jember , Kode Pos 68172</font></p>
-        <form class="form-inline">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
+       
+ 
+      </nav>
+      <div id="menu">
+        <ul>
+            <li><a href="nyoba2.php">Beranda</a></li>
+            <li><a href="profil.html">Profil Desa</a></li>
+            <li><a href="Berita">Berita</a></li>
+            <li><a href="#">Pelayanan</a></li>
+            <li><a href="Petunjuk">Petunjuk</a></li>
+
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <ul class="nav navbar-nav navbar-right">
         <?php
    ob_start();
     session_start();
@@ -92,20 +53,11 @@ function toggleSidebar(){
     }
                 ?> 
               </ul>
-      </nav>
-      <div id="sidebar">
-          <div class="toggle-btn" onclick="toggleSidebar()"><a>
-           <span></span>
-           <span></span>
-           <span></span>
-          </a>
-          </div>
-          <ul>
-           <li><a href="profil.html">Profil Desa</a></li>
-           <li>Sell</li>
-           <li>Buy</li>
+              </form>
+ 
          
-         </div>
+        </ul>
+    </div>
       <div class=malasngoding-slider>
           <div class=isi-slider>
             <img src="sabrang.jpg" alt="Gambar 1" height="100" width="100">
