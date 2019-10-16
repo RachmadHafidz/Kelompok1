36f89a065efbfb9209ec85624da2c25d987f1927
rianjanuarian <55471319+rianjanuarian@users.noreply.github.com>
2019-10-16 08:57:33 +0700

    Create proses_hapus.php

---
+++ rian/selasa/crud/proses_hapus.php
@@ -0,0 +1,28 @@
+<?php
+// Load file koneksi.php
+include "koneksi.php";
+
+// Ambil data NIK yang dikirim oleh index.php melalui URL
+$NIK = $_GET['NIK'];
+
+// Query untuk menampilkan data siswa berdasarkan NIS yang dikirim
+$query = "SELECT * FROM p_desa WHERE NIK='".$NIK."'";
+$sql = mysqli_query($connect, $query); // Eksekusi/Jalankan query dari variabel $query
+$data = mysqli_fetch_array($sql); // Ambil data dari hasil eksekusi $sql
+
+// Cek apakah file fotonya ada di folder images
+if(is_file("images/".$data['foto'])) // Jika foto ada
+	unlink("images/".$data['foto']); // Hapus foto yang telah diupload dari folder images
+
+// Query untuk menghapus data siswa berdasarkan NIS yang dikirim
+$query2 = "DELETE FROM p_desa WHERE NIK='".$NIK."'";
+$sql2 = mysqli_query($connect, $query2); // Eksekusi/Jalankan query dari variabel $query
+
+if($sql2){ // Cek jika proses simpan ke database sukses atau tidak
+	// Jika Sukses, Lakukan :
+	header("location: index.php"); // Redirect ke halaman index.php
+}else{
+	// Jika Gagal, Lakukan :
+	echo "Data gagal dihapus. <a href='index.php'>Kembali</a>";
+}
+?>
