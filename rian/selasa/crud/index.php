46663f4e5a1d74ce1cf23c984ce3aebdda126bf3
rianjanuarian <55471319+rianjanuarian@users.noreply.github.com>
2019-10-16 08:57:14 +0700

    Create index.php

---
+++ rian/selasa/crud/index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<title>Perangkat Desa</title>
+</head>
+<body>
+	<h1>Perangkat Desa</h1>
+	<a href="form_simpan.php">Tambah Data</a><br><br>
+	<table border="1" width="100%">
+	<tr>
+        
+        <th>Foto</th>
+		<th>NIK</th>
+		<th>Nama</th>
+		<th>Alamat</th>
+		<th>Jabatan</th>
+
+	
+		<th colspan="2">Aksi</th>
+	</tr>
+	<?php
+	// Load file koneksi.php
+	include "koneksi.php";
+	
+	$query = "SELECT * FROM p_desa"; // Query untuk menampilkan semua data siswa
+	$sql = mysqli_query($connect, $query); // Eksekusi/Jalankan query dari variabel $query
+	
+	while($data = mysqli_fetch_array($sql)){ // Ambil semua data dari hasil eksekusi $sql
+        echo "<tr>";
+        echo "<td><img src='images/".$data['foto']."' width='100' height='100'></td>";
+		echo "<td>".$data['NIK']."</td>";
+		echo "<td>".$data['nama']."</td>";
+		echo "<td>".$data['alamat']."</td>";
+		echo "<td>".$data['jabatan']."</td>";
+      
+		echo "<td><a href='form_ubah.php?NIK=".$data['NIK']."'>Ubah</a></td>";
+		echo "<td><a href='proses_hapus.php?NIK=".$data['NIK']."'>Hapus</a></td>";
+		echo "</tr>";
+	}
+	?>
+	</table>
+</body>
+</html>
