490a2793b6af027f6d8b578d3907d002a85a3eef
rianjanuarian <55471319+rianjanuarian@users.noreply.github.com>
2019-10-23 14:06:44 +0700

    Update nyoba2.php

--- rian/selasa/nyoba2.php
+++ rian/selasa/nyoba2.php
@@ -12,6 +12,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
  <style>
+   
   .dropbtn {
   background: none;
   color: rgb(0, 0, 0);
@@ -64,7 +65,7 @@
         
       <nav class="navbar navbar-dark bg-dark justify-content-between"> <p><a><font class="navbar-brand" color="white">Sistem Informasi Desa Sabrang</font></a></p>
         <p><font color="white">Jl. watu ulo no 1, Desa Sabrang, Kec. Ambulu, Kab. Jember , Kode Pos 68172</font></p>
-       
+        <a href="nyoba.html">Logout</a>
  
       </nav>
 </div>
@@ -101,6 +102,7 @@
         <i class="fa fa-caret-down"></i>
       </button>
   </div>
+ 
   
   <div class="dropdown">
         <button class="dropbtn"><a href="#">Pelayanan</a>
@@ -134,7 +136,8 @@ echo "Hai, selamat datang ". $_SESSION['username'];
 ?>
 <br/>
 <br/>
-<a href="logout.php">LOGOUT</a>
+
+
     
   
      
