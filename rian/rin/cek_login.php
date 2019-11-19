0e365ede10709b605964ddce68f0fcfa5580aa59
inirijall66 <55471461+inirijall66@users.noreply.github.com>
2019-11-19 18:50:47 +0700

    Update cek_login.php

--- rian/rin/cek_login.php
+++ rian/rin/cek_login.php
@@ -11,7 +11,7 @@ $PASSWORD = $_POST['PASSWORD'];
  
  
 // menyeleksi data user dengan username dan password yang sesuai
-$login = mysqli_query($koneksi,"select * from warga where USERNAME='$USERNAME' and PASSWORD='$PASSWORD'");
+$login = mysqli_query($koneksi,"select * from admin where USERNAME='$USERNAME' and PASSWORD='$PASSWORD'");
 // menghitung jumlah data yang ditemukan
 $cek = mysqli_num_rows($login);
  
