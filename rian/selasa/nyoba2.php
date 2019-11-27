a923b128a333590254c427417bbc7e372fd339c3
inirijall66 <55471461+inirijall66@users.noreply.github.com>
2019-11-27 10:29:52 +0700

    Update nyoba2.php

--- rian/selasa/nyoba2.php
+++ rian/selasa/nyoba2.php
@@ -110,7 +110,7 @@
         </button>
         <div class="dropdown-content">
           <a href="#">Persyaratan</a>
-          <a href="#">Pelayanan Surat Domisili</a>
+          <a href="formindex.php">Pelayanan Surat Domisili</a>
           <a href="#">Pelayanan Surat SKCK</a>
           <a href="#">Pelyanan Surat Belum Menikah</a>
           <a href="#">Pelayanan Surat Tempat Usaha</a>
