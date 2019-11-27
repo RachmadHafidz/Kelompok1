5ca93d4466ead036bcf544ef9aa5af62ace1e3c4
inirijall66 <55471461+inirijall66@users.noreply.github.com>
2019-11-27 10:28:56 +0700

    Update formindex.php

--- rian/selasa/formindex.php
+++ rian/selasa/formindex.php
@@ -10,62 +10,55 @@
 
 <div class="container">
             <form class="form-horizontal" role="form">
-                <h2>Registration</h2>
+                <h2>Pelayanan</h2>
                 <div class="form-group">
-                    <label for="firstName" class="col-sm-3 control-label">First Name</label>
+                    <label for="firstName" class="col-sm-3 control-label">Nama</label>
                     <div class="col-sm-9">
-                        <input type="text" id="firstName" placeholder="First Name" class="form-control" autofocus>
+                        <input type="text" id="firstName" placeholder="Nama" class="form-control" autofocus>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="lastName" class="col-sm-3 control-label">Last Name</label>
+                    <label for="lastName" class="col-sm-3 control-label">NIk</label>
                     <div class="col-sm-9">
-                        <input type="text" id="lastName" placeholder="Last Name" class="form-control" autofocus>
+                        <input type="text" id="lastName" placeholder="NIK" class="form-control" autofocus>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="email" class="col-sm-3 control-label">Email* </label>
+                    <label for="email" class="col-sm-3 control-label">Alamat </label>
                     <div class="col-sm-9">
-                        <input type="email" id="email" placeholder="Email" class="form-control" name= "email">
+                        <input type="email" id="email" placeholder="Alamat" class="form-control" name= "email">
                     </div>
                 </div>
+
                 <div class="form-group">
-                    <label for="password" class="col-sm-3 control-label">Password*</label>
+                    <label for="email" class="col-sm-3 control-label">Tempat Lahir </label>
                     <div class="col-sm-9">
-                        <input type="password" id="password" placeholder="Password" class="form-control">
+                        <input type="email" id="email" placeholder="Tempat Lahir" class="form-control" name= "email">
                     </div>
                 </div>
+                
                 <div class="form-group">
-                    <label for="password" class="col-sm-3 control-label">Confirm Password*</label>
-                    <div class="col-sm-9">
-                        <input type="password" id="password" placeholder="Password" class="form-control">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="birthDate" class="col-sm-3 control-label">Date of Birth*</label>
+                    <label for="birthDate" class="col-sm-3 control-label">Tanggal Lahir</label>
                     <div class="col-sm-9">
                         <input type="date" id="birthDate" class="form-control">
                     </div>
                 </div>
+
                 <div class="form-group">
-                    <label for="phoneNumber" class="col-sm-3 control-label">Phone number </label>
+                    <label for="birthDate" class="col-sm-3 control-label">Tanggal Surat</label>
+                    <div class="col-sm-9">
+                        <input type="date" id="birthDate" class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="phoneNumber" class="col-sm-3 control-label"></label>
                     <div class="col-sm-9">
                         <input type="phoneNumber" id="phoneNumber" placeholder="Phone number" class="form-control">
                         <span class="help-block">Your phone number won't be disclosed anywhere </span>
                     </div>
                 </div>
-                <div class="form-group">
-                        <label for="Height" class="col-sm-3 control-label">Height* </label>
-                    <div class="col-sm-9">
-                        <input type="number" id="height" placeholder="Please write your height in centimetres" class="form-control">
-                    </div>
-                </div>
-                 <div class="form-group">
-                        <label for="weight" class="col-sm-3 control-label">Weight* </label>
-                    <div class="col-sm-9">
-                        <input type="number" id="weight" placeholder="Please write your weight in kilograms" class="form-control">
-                    </div>
-                </div>
+               
                 <div class="form-group">
                     <label class="control-label col-sm-3">Gender</label>
                     <div class="col-sm-6">
