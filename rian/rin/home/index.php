43fa2a596091582ac94c064cf8f681abf52c2f43
inirijall66 <55471461+inirijall66@users.noreply.github.com>
2019-12-03 09:34:02 +0700

    Update index.php

--- rian/rin/home/index.php
+++ rian/rin/home/index.php
@@ -78,7 +78,7 @@
         <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
           <li class="user-profile-data">
           <span class="userpicture"><i title="Profile icon" class=" fa fa-user-circle-o"></i></span>
-          <div <a class="dropdown-item" href="profil.php"><?php session_start(); echo $_SESSION['NIKADMIN']; ?></a></div>
+          <div <a class="dropdown-item" href="profil.php"><?php session_start(); echo $_SESSION['USERNAME']; ?></a></div>
           </li>
           <div class="dropdown-divider"></div>
           <li> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a></li>
