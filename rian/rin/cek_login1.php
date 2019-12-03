2e8c49c498bb319201b65446c8b3743e0af561a0
inirijall66 <55471461+inirijall66@users.noreply.github.com>
2019-12-03 08:52:05 +0700

    Update cek_login1.php

--- rian/rin/cek_login1.php
+++ rian/rin/cek_login1.php
@@ -6,39 +6,34 @@ session_start();
 include 'koneksi.php';
  
 // menangkap data yang dikirim dari form login
-$NIK = $_POST['NIKADMIN'];
-$PASSWORD= md5($_POST['PASSWORD']);
+$NIK = $_POST['NIK_PENDUDUK'];
+$PASSWORD= md5($_POST['PASS']);
  
  
 // menyeleksi data user dengan username dan password yang sesuai
-$login = mysqli_query($koneksi,"select * from admin where NIKADMIN='$NIK' and PASSWORD='$PASSWORD'");
+$login = mysqli_query($koneksi,"select * from penduduk where NIK_PENDUDUK='$NIK' and PASS='$PASSWORD'");
+
+
 // menghitung jumlah data yang ditemukan
 $cek = mysqli_num_rows($login);
  
 // cek apakah username dan password di temukan pada database
-if($cek > 0){
+if($cek > 0)
+{
  
 	$data = mysqli_fetch_assoc($login);
- 
+	
+            
+
+	header("location:http://localhost/Kelompok1/rian/selasa/nyoba2.php");
 	// cek jika user login sebagai admin
-	if($data['LEVEL']=="warga"){
- 
-		// buat session login dan username
-		$_SESSION['NIK'] = $NIK;
-		$_SESSION['LEVEL'] = "warga";
-		// alihkan ke halaman dashboard admin
-        header("location:http://localhost/Kelompok1/rian/selasa/nyoba2.php");
+	
+	
+}
+else
+	{
+	header("location:index1.php?pesan=gagal");
+	}
  
 	
- 
- 
-	}else{
- 
-		// alihkan ke halaman login kembali
-		header("location:index1.php?pesan=gagal");
-	}	
-}else{
-	header("location:index1.php?pesan=gagal");
-}
- 
 ?>
