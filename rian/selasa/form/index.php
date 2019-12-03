a0064d83367325826448b7caa3761c3f8670f60b
inirijall66 <55471461+inirijall66@users.noreply.github.com>
2019-12-03 16:22:58 +0700

    Update index.php

--- rian/selasa/form/index.php
+++ rian/selasa/form/index.php
@@ -3,19 +3,16 @@
 	<title></title>
 </head>
 <body>
-	
+	<?php include "koneksi1.php";?>
 	<h1>Data Surat Domisili</h1>
 	<a href="formindex.php">Tambah Data</a><br><br>
 	<?php session_start (); 
 	
-	echo $_SESSION['NIK_PENDUDUK'];
+echo $_SESSION['NIK_PENDUDUK'];
 
-	include "koneksi1.php";
-	$nik=$_SESSION['NIK_PENDUDUK'];
-	$query = "SELECT * FROM penduduk inner join sp_domisili on penduduk.NIK_PENDUDUK=sp_domisili.NIK_PENDUDUK where NIK_PENDUDUK='$nik'"; // Query untuk menampilkan semua data siswa
-	$sql = mysqli_query($connect,$query); // Eksekusi/Jalankan query dari variabel $query
-	$data = mysqli_fetch_array($sql); 
-	?>
+	
+ ?>
+
 	</br>
 	<table border="1" width="100%">
 	<tr>
@@ -24,33 +21,45 @@
 		<th>tanggal surat</th>
         <th>nama penduduk</th>
         <th>jk peng</th>
-        <th>agama peng</th>
-        <th>nik peng</th>
+        <th>agama</th>
+        <th>NIKPENGAJU</th>
         <th>tmp lahir peng</th>
         <th>tgl lahir peng</th>
         <th>Pekerjaan peng</th>
         <th>alamat peng</th>
 		<th>tujuan peng</th>
+		<th>KETERANGAN</th>
+		<th>JENIS SURAT</th>
 		<th colspan="2">Aksi</th>
 	</tr>
 	<?php
-	// Load file koneksi.php// Ambil semua data dari hasil eksekusi $sql
-		echo "<tr>";
-		echo "<td>".$data['NO_SURATDOM']."</td>";
-		echo "<td>".$data['NIK_PENDUDUK']."</td>";
-		echo "<td>".$data['TANGGAL_SURAT']."</td>";
-        echo "<td>".$data['NAMA_PENDUDUK']."</td>";
-        echo "<td>".$data['JK_PENGAJU']."</td>";
-        echo "<td>".$data['AGAMA_PENGAJU']."</td>";
-        echo "<td>".$data['NIK_PENGAJU']."</td>";
-        echo "<td>".$data['TMP_LAHIRPENGAJU']."</td>";
-        echo "<td>".$data['TGL_LAHIRPENGAJU']."</td>";
-        echo "<td>".$data['PEKERJAANPENGAJU']."</td>";
-		echo "<td>".$data['ALAMATPENGAJU']."</td>";
-		echo "<td>".$data['TUJUAN']."</td>";
-		
-		echo "</tr>";
+	// Load file koneksi.php
 	
+	
+	$nik= $_SESSION['NIK_PENDUDUK'];
+$nilai = mysqli_query($connect,"select * from sk_domisili inner join penduduk on sp_domisili.NIK_PENDUDUK = penduduk.NIK_PENDUDUK where NIK_PENDUDUK= $nik");
+
+
+	while($data = mysqli_fetch_array($nilai)){ // Ambil semua data dari hasil eksekusi $sql
+		echo "<tr>";
+		echo "<td>".$data['NO_DOMISILI']."</td>";
+		echo "<td>".$data['NIK_PENDUDUK']."</td>";
+		echo "<td>".$data['TGLSURATAJU']."</td>";
+        echo "<td>".$data['NAMAAJU']."</td>";
+        echo "<td>".$data['JKAJU']."</td>";
+        echo "<td>".$data['AGAMAAJU']."</td>";
+        echo "<td>".$data['NIKPENGAJU']."</td>";
+        echo "<td>".$data['TMPLHRAJU']."</td>";
+        echo "<td>".$data['TGLHRAJU']."</td>";
+        echo "<td>".$data['PEKERJAAN_AJU']."</td>";
+		echo "<td>".$data['ALAMATAJU']."</td>";
+		echo "<td>".$data['TUJUANAJU']."</td>";
+		echo "<td>".$data['KETERANGANAJU']."</td>";
+		echo "<td>".$data['JENIS_SURATAJU']."</td>";
+		echo "<td><a href='form_ubah.php?NIK=".$data['NIK_PENDUDUK']."'>Ubah</a></td>";
+		echo "<td><a href='proses_hapus.php?NIK=".$data['NIK_PENDUDUK']."'>Hapus</a></td>";
+		echo "</tr>";
+	}
 	?>
 	</table>
 	<a href="http://localhost/Kelompok1/rian/selasa/nyoba2.php">Kembali</a>
