654c9a03bb970c936a853cdbaf2ddf6a17e4fb59
inirijall66 <55471461+inirijall66@users.noreply.github.com>
2019-12-03 10:48:23 +0700

    Update index.php

--- rian/selasa/form/index.php
+++ rian/selasa/form/index.php
@@ -3,15 +3,27 @@
 	<title></title>
 </head>
 <body>
+	
 	<h1>Data Surat Domisili</h1>
 	<a href="formindex.php">Tambah Data</a><br><br>
+	<?php session_start (); 
+	
+	echo $_SESSION['NIK_PENDUDUK'];
+
+	include "koneksi1.php";
+	$nik=$_SESSION['NIK_PENDUDUK'];
+	$query = "SELECT * FROM penduduk inner join sp_domisili on penduduk.NIK_PENDUDUK=sp_domisili.NIK_PENDUDUK where NIK_PENDUDUK='$nik'"; // Query untuk menampilkan semua data siswa
+	$sql = mysqli_query($connect,$query); // Eksekusi/Jalankan query dari variabel $query
+	$data = mysqli_fetch_array($sql); 
+	?>
+	</br>
 	<table border="1" width="100%">
 	<tr>
 		<th>No surat</th>
 		<th>nik PENDUDUK</th>
 		<th>tanggal surat</th>
-        <th>nama pengaju</th>
-        <th>jk peng/th>
+        <th>nama penduduk</th>
+        <th>jk peng</th>
         <th>agama peng</th>
         <th>nik peng</th>
         <th>tmp lahir peng</th>
@@ -22,18 +34,12 @@
 		<th colspan="2">Aksi</th>
 	</tr>
 	<?php
-	// Load file koneksi.php
-	include "koneksi1.php";
-	
-	$query = "SELECT * FROM sp_domisili"; // Query untuk menampilkan semua data siswa
-	$sql = mysqli_query($connect, $query); // Eksekusi/Jalankan query dari variabel $query
-	
-	while($data = mysqli_fetch_array($sql)){ // Ambil semua data dari hasil eksekusi $sql
+	// Load file koneksi.php// Ambil semua data dari hasil eksekusi $sql
 		echo "<tr>";
 		echo "<td>".$data['NO_SURATDOM']."</td>";
 		echo "<td>".$data['NIK_PENDUDUK']."</td>";
 		echo "<td>".$data['TANGGAL_SURAT']."</td>";
-        echo "<td>".$data['NAMA_PENGAJU']."</td>";
+        echo "<td>".$data['NAMA_PENDUDUK']."</td>";
         echo "<td>".$data['JK_PENGAJU']."</td>";
         echo "<td>".$data['AGAMA_PENGAJU']."</td>";
         echo "<td>".$data['NIK_PENGAJU']."</td>";
@@ -44,7 +50,7 @@
 		echo "<td>".$data['TUJUAN']."</td>";
 		
 		echo "</tr>";
-	}
+	
 	?>
 	</table>
 	<a href="http://localhost/Kelompok1/rian/selasa/nyoba2.php">Kembali</a>
