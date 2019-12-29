9049dc4c8bec0eab7ecfbdf4e177ba4aab511686
FadhilaDwi <fadhiladwikurniawan@gmail.com>
2019-12-29 22:33:35 +0700

    Update petunjukwarga.php

--- sidsabrang/petunjukwarga.php
+++ sidsabrang/petunjukwarga.php
@@ -3,7 +3,8 @@
 <head>
 </head>
 <body>
-<img src="malasngoding.png">
+<center> <h1> PETUNJUK WEBSITE </h1>
+<img src="http://localhost/Kelompok1/sidsabrang/img/Petunjuk/petunjukwarga.PNG"></center>
 </body>
 </html>
 
