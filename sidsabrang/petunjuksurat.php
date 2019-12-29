8bc772af3c10bba3bc1c18a89e1c622c1a8249f1
FadhilaDwi <fadhiladwikurniawan@gmail.com>
2019-12-29 23:22:20 +0700

    Update petunjuksurat.php

---
+++ sidsabrang/petunjuksurat.php
@@ -0,0 +1,9 @@
+<html>
+<title> </title>
+<head>
+</head>
+<body>
+<center> <h1> PETUNJUK SURAT </h1>
+<img src="http://localhost/Kelompok1/sidsabrang/img/Petunjuk/petunjuksurat.PNG"></center>
+</body>
+</html>
