43850adf2fdc2c17a9b2cbf89561867bcfe1f223
rianjanuarian <55471319+rianjanuarian@users.noreply.github.com>
2020-08-08 21:43:48 +0700

    Update index.php

--- sidsabrang/index.php
+++ sidsabrang/index.php
@@ -506,7 +506,7 @@ include 'koneksi.php';
         </div>
 
    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  Sabrang © 2020 | Pemerintah Kabupaten Jember
    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 
     </footer>
